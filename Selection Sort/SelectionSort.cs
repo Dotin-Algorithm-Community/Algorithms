edb3a514579924bfe329da6cc579f5f78066748c
Mostafa Koolabadi <draxsis.1995@gmail.com>
2024-07-18 19:14:54 +0330

    Update SelectionSort.cs

--- Selection Sort/SelectionSort.cs
+++ Selection Sort/SelectionSort.cs
@@ -1,21 +1,38 @@
-public void SelectionSort()
+class public Program
 {
-    var arrayLength = NumArray.Length;
 
-    for (int i = 0; i < arrayLength - 1; i++)
+    public static void SelectionSort(int[] array)
     {
-        var minimum = i;
-
-        for (int j = i + 1; j < arrayLength; j++)
+        var arrayLength = array.Length;
+    
+        for (int i = 0; i < arrayLength - 1; i++)
         {
-            if (NumArray[j] < NumArray[minimum])
+            var minimum = i;
+    
+            for (int j = i + 1; j < arrayLength; j++)
             {
-                minimum = j;
+                if (NumArray[j] < NumArray[minimum])
+                {
+                    minimum = j;
+                }
             }
+    
+            var temp = NumArray[minimum];
+            NumArray[minimum] = NumArray[i];
+            NumArray[i] = temp;
         }
+    }
 
-        var temp = NumArray[minimum];
-        NumArray[minimum] = NumArray[i];
-        NumArray[i] = temp;
+    public static void Main()
+    {
+        int[] array = {3,4,12,52,74,3,72,41};
+        
+        SelectionSort(array);
+
+        foreach(var item in array)
+        {
+            Console.WriteLine(item)
+        }
     }
 }
+
